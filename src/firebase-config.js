78db360124934b87c91df848c924a2288ee180bb
sam4ntha <stap0428@gmail.com>
2022-11-20 01:47:10 -0600

    Ya jaló el inicio de sesión

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -6,15 +6,13 @@
  * 3. Select Config from the Firebase SDK snippet pane.
  * 4. Copy the config object snippet, then add it here.
  */
-const config = {
-  apiKey: "API_KEY",
-  authDomain: "PROJECT_ID.firebaseapp.com",
-  databaseURL: "https://PROJECT_ID.firebaseio.com",
-  projectId: "PROJECT_ID",
-  storageBucket: "PROJECT_ID.appspot.com",
-  messagingSenderId: "SENDER_ID",
-  appId: "APP_ID",
-  measurementId: "G-MEASUREMENT_ID",
+ const config = {
+    apiKey: "AIzaSyA9RqtTyR6tT-z5cz1R7Z_9HS0XQC202Lk",
+    authDomain: "friendlychat-675fd.firebaseapp.com",
+    projectId: "friendlychat-675fd",
+    storageBucket: "friendlychat-675fd.appspot.com",
+    messagingSenderId: "741738517137",
+    appId: "1:741738517137:web:b93ad2be5418c530006530"
 };
 
 export function getFirebaseConfig() {
@@ -25,21 +23,3 @@ export function getFirebaseConfig() {
     return config;
   }
 }
-
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyA9RqtTyR6tT-z5cz1R7Z_9HS0XQC202Lk",
-  authDomain: "friendlychat-675fd.firebaseapp.com",
-  projectId: "friendlychat-675fd",
-  storageBucket: "friendlychat-675fd.appspot.com",
-  messagingSenderId: "741738517137",
-  appId: "1:741738517137:web:b93ad2be5418c530006530"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
